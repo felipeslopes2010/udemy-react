a0d2ebb86589c29f6f8859367fd9918810956640
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-06-05 20:06:16 -0300

    Create First Component

---
+++ fundamentos-react/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { FirstComponent } from './components/FirstComponent'
+
+const App = () => (
+  <div className='App'>
+    <h1>Fundamentos React</h1>
+    <FirstComponent />
+  </div>
+)
+
+export { App }
---
+++ fundamentos-react/src/components/FirstComponent.jsx
@@ -0,0 +1,7 @@
+const FirstComponent = () => (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+)
+
+export { FirstComponent };
